c178f219d40a735c141a2c74a8bb578676fa99ae
Gustavo M T Borges <gustavo.mtborges@gmail.com>
2017-11-07 11:18:58 -0200

    Adicionar features ES7

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 module.exports = {
-  entry: './src/js/main.js',
+  entry: ['./src/js/main.js'],
   output: {
     filename: './public/js/bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules)/
+      }
+    ]
   }
 }
