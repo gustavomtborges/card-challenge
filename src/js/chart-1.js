ad30bc902502ab66e826914b0b371691b23f6ef5
Gustavo M T Borges <gustavo.mtborges@gmail.com>
2017-11-07 01:53:43 -0200

    Adicionar primeiros charts utilizando highcharts

---
+++ src/js/chart-1.js
@@ -0,0 +1,30 @@
+const Highcharts = require('highcharts');
+require('highcharts/modules/exporting')(Highcharts);
+
+function chart1() {
+  Highcharts.chart('card-1', {
+    chart: {
+      type: 'bar'
+    },
+    title: {
+      text: 'Fruit Consumption'
+    },
+    xAxis: {
+      categories: ['Apples', 'Bananas', 'Oranges']
+    },
+    yAxis: {
+      title: {
+        text: 'Fruit eaten'
+      }
+    },
+    series: [{
+      name: 'Jane',
+      data: [1, 0, 4]
+    }, {
+      name: 'John',
+      data: [5, 7, 3]
+    }]
+  });
+};
+
+export default chart1;
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import chart1 from './chart-1';
+import chart2 from './chart-2';
+import chart3 from './chart-3';
+
+window.onload = () => {
+  chart1();
+  chart2();
+  chart3();
+}
